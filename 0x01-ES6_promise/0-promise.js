0d79660906d656f1bbf164cfdb42de6d8f5ee49b
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-10-03 15:39:01 +0000

    0. Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,3 @@
-//promise.js
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulate an API call that is always successful
-        const success = true;  // This can be any condition
-        
-        if (success) {
-            resolve('API response received');
-        } else {
-            reject('Error occurred while fetching data');
-        }
-    });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
