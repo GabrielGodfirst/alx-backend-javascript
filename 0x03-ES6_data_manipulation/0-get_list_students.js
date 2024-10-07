ba3cb855357f330d5151cbfb5ca1b97a8357fdf4
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-10-07 13:48:57 +0000

    0. Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const A = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const B = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const C = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [A, B, C];
+}
